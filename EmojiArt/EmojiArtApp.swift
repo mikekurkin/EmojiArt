6338883a5ed587ac19bc5349cde6a1ce75760782
Mike Kurkin <mike@kurkin.net>
2020-10-11 17:25:41 +0300

    Initial Commit

---
+++ EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Mike Kurkin on 11.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
